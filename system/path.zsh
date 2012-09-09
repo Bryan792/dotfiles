4afe8cc5249334511a8e7d57a495b6cbb20d6a18
Devin Snyder <devinsnyder@gmail.com>
2012-09-08 17:10:54 -0700

    Added JAVA_HOME

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
 export PATH="$HOME/bin:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64"
