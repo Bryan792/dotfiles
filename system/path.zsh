6978440adfbd638e78835653c801d8c0326a4ab3
Devin Snyder <devinsnyder@gmail.com>
2012-09-28 19:47:00 -0700

    added android for class development

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH="$HOME/bin:./bin:$HOME/.rbenv/shims:$HOME/.rbenv/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$DOTFILES/bin:$PATH"
+export PATH="$HOME/bin:./bin:$HOME/.rbenv/shims:$HOME/.rbenv/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$DOTFILES/bin:$PATH:/home/dsnyder/android-sdk-linux/tools"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
