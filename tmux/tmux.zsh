f54b4c4e7f367a85220eb682440c46635a81cc26
Bryan Ching <bryan792@gmail.com>
2012-11-06 13:58:50 -0800

    Fixed tmux for now

--- tmux/tmux.zsh
+++ tmux/tmux.zsh
@@ -1,8 +1,10 @@
 # Default to a split-screen tmux
 # session when opening a shell.
 
-if $(which tmux &>/dev/null)
-then
-  [[ $TERM != "screen" ]] && exec tmux
-  compctl -g '~/.teamocil/*(:t:r)' teamocil
-fi
+alias tmux="TERM=xterm-256color tmux"
+
+#if $(which tmux &>/dev/null)
+#then
+#  [[ $TERM != "screen" ]] && exec tmux
+#  compctl -g '~/.teamocil/*(:t:r)' teamocil
+#fi
