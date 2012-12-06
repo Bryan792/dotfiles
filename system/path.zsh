d9d5ebb01d108d196c569f0613418aaf89c16686
Bryan Ching <bryan792@gmail.com>
2012-12-05 22:07:31 -0800

    Added Latex

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH="$HOME/bin:./bin:$HOME/.rbenv/shims:$HOME/.rbenv/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$DOTFILES/bin:$PATH:/home/dsnyder/android-sdk-linux/tools"
+export PATH="$HOME/bin:./bin:$HOME/.rbenv/shims:$HOME/.rbenv/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$DOTFILES/bin:$PATH:/home/dsnyder/android-sdk-linux/tools:/usr/local/texlive/2012/bin/x86_64-linux/"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
