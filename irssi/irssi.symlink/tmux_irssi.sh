2c50708ec48ea43349dcb8a5051ff6d6a9a79d15
Bryan Ching <bryan792@gmail.com>
2013-04-30 03:36:38 -0700

    Lots of changes, arch laptop pretty much setup

--- irssi/irssi.symlink/tmux_irssi.sh
+++ irssi/irssi.symlink/tmux_irssi.sh
@@ -3,24 +3,26 @@
 T3=$(pidof irssi)
 
 irssi_nickpane() {
-    tmux setw main-pane-width $(( 168 - 25));
-    tmux splitw -v "cut -c -25 ~/.irssi/nicklistfifo";
-    tmux selectl main-vertical;
-    tmux selectw -t irssi;
-    tmux selectp -t 0;
+  tmux setw main-pane-width $(( $(tput cols) - 26));
+  tmux send-keys -t irssi "tmux send-keys -t0 \"irssi\" C-m; \ 
+  tmux send-keys -t0 \"/set nicklist_height $(tput lines)\" C-m" C-m 
+  tmux splitw -v "cut -c -25 ~/.irssi/nicklistfifo";
+  tmux selectl main-vertical;
+  tmux selectw -t irssi;
+  tmux selectp -t 0;
 }
 
 irssi_repair() {
-    tmux selectw -t irssi
-    (( $(tmux lsp | wc -l) > 1 )) && tmux killp -a -t 0
-    irssi_nickpane
+  tmux selectw -t irssi
+  (( $(tmux lsp | wc -l) > 1 )) && tmux killp -a -t 0
+  irssi_nickpane
 }
 
 if [ -z "$T3" ]; then
-    tmux new-session -d -s main;
-    tmux new-window -t main -n irssi irssi;
-    irssi_nickpane ;
+  tmux new-session -d -s main;
+  tmux new-window -t main -n irssi;
+  irssi_nickpane ;
 fi
-    tmux attach-session -d -t main;
-    irssi_repair ;
+tmux attach-session -d -t main;
+irssi_repair ;
 exit 0
