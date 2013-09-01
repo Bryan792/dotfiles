8848b05788f5ff96d69c7bfcc1c1ea64a343f23a
Bryan Ching <bryan@256.bryanching.net>
2013-09-01 15:22:59 -0700

    Update after usage in vps

--- android.zsh
+++ android.zsh
@@ -1,2 +1,3 @@
 export ANDROID_HOME=/home/bryan/Downloads/android/adt-bundle-linux-x86_64/adt-bundle-linux-x86_64/sdk
 path=($path $ANDROID_HOME/tools $ANDROID_HOME/platform-tools /home/bryan/Downloads/sbt/bin) 
+alias python2=python2.6
