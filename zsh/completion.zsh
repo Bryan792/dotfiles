759a4ca29d36f0be4382f2d42662e29306539747
Bryan Ching <bryan792@gmail.com>
2026-09-11 21:58:24 -0700

    Restore substring path completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,5 +1,8 @@
-# matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+# Try case-insensitive matching first, then partial-word and substring matches.
+zstyle ':completion:*' matcher-list \
+  'm:{a-zA-Z}={A-Za-z}' \
+  'r:|[._-]=* r:|=*' \
+  'l:|=* r:|=*'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
